bdeb31ff9d3817abe1854482b94fcb710968d627
paperbenni <paperbenni@gmail.com>
2018-07-30 19:02:46 +0200

    Update c2r.sh

--- c2r.sh
+++ c2r.sh
@@ -1,16 +1,28 @@
 c2r(){
-if [ -e "$1" ]
-then
-  if ffprobe ./"$1"
+  
+  if [ -e "$1" ]
   then
-    echo "file is video"
-  else
-    exit
+  
+    if ffprobe ./"$1"
+    then
+      echo "file is video"
+    else
+      exit
   fi
-ffmpeg -y -i "$1" -c:v mpeg4 -r ntsc-film -b:v 250000k -c:a pcm_s16le "${1%.*}.mov"
-else
-echo "file does not exist!"
-fi
+
+    if [ -e "${1%.*}.mov" ] || [ -e ./resolve/"${1%.*}.mov" ]
+    then
+      echo "skipping $1"
+    else
+      ffmpeg -y -i "$1" -c:v mpeg4 -r ntsc-film -b:v 250000k -c:a pcm_s16le "${1%.*}.mov"
+      if [ -e ./resolve ]
+      then
+        mv "${1%.*}.mov" resolve/"${1%.*}.mov"
+      fi 
+    fi
+  else
+    echo "file does not exist!"
+  fi
 }
 
 batchc2r(){
